1c78c83000f96b44db5589fa5b88cd2e6ce3c6b1
Rolf Christian Jørgensen <27211248+rcjorgensen@users.noreply.github.com>
2024-03-22 23:19:35 +0100

    Rename consts

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const CANVAS_WIDTH = 800;
 const CANVAS_HEIGHT = 600;
 
-const RECT = {
+const rect = {
   x: 350,
   y: 260,
   width: 200,
   height: 160,
 };
 
-const CIRCLE = {
+const circle = {
   x: 300,
   y: 300,
   radius: 120,
@@ -26,63 +26,63 @@ draw();
 const inputCircleX = document.getElementById("circle-x");
 inputCircleX.min = -CANVAS_WIDTH;
 inputCircleX.max = CANVAS_WIDTH;
-inputCircleX.value = CIRCLE.x;
+inputCircleX.value = circle.x;
 inputCircleX.oninput = (e) => {
-  CIRCLE.x = parseInt(e.target.value);
+  circle.x = parseInt(e.target.value);
   draw();
 };
 
 const inputCircleY = document.getElementById("circle-y");
 inputCircleY.min = -CANVAS_HEIGHT;
 inputCircleY.max = CANVAS_HEIGHT;
-inputCircleY.value = CIRCLE.y;
+inputCircleY.value = circle.y;
 inputCircleY.oninput = (e) => {
-  CIRCLE.y = parseInt(e.target.value);
+  circle.y = parseInt(e.target.value);
   draw();
 };
 
 const inputRadius = document.getElementById("radius");
 inputRadius.min = 0;
 inputRadius.max = Math.max(CANVAS_WIDTH, CANVAS_HEIGHT);
-inputRadius.value = CIRCLE.radius;
+inputRadius.value = circle.radius;
 inputRadius.oninput = (e) => {
-  CIRCLE.radius = parseInt(e.target.value);
+  circle.radius = parseInt(e.target.value);
   draw();
 };
 
 const inputRectX = document.getElementById("rect-x");
 inputRectX.min = -CANVAS_WIDTH;
 inputRectX.max = CANVAS_WIDTH;
-inputRectX.value = RECT.x;
+inputRectX.value = rect.x;
 inputRectX.oninput = (e) => {
-  RECT.x = parseInt(e.target.value);
+  rect.x = parseInt(e.target.value);
   draw();
 };
 
 const inputRectY = document.getElementById("rect-y");
 inputRectY.min = -CANVAS_HEIGHT;
 inputRectY.max = CANVAS_HEIGHT;
-inputRectY.value = RECT.y;
+inputRectY.value = rect.y;
 inputRectY.oninput = (e) => {
-  RECT.y = parseInt(e.target.value);
+  rect.y = parseInt(e.target.value);
   draw();
 };
 
 const inputWidth = document.getElementById("width");
 inputWidth.min = -CANVAS_WIDTH;
 inputWidth.max = CANVAS_WIDTH;
-inputWidth.value = RECT.width;
+inputWidth.value = rect.width;
 inputWidth.oninput = (e) => {
-  RECT.width = parseInt(e.target.value);
+  rect.width = parseInt(e.target.value);
   draw();
 };
 
 const inputHeight = document.getElementById("height");
 inputHeight.min = -CANVAS_HEIGHT;
 inputHeight.max = CANVAS_HEIGHT;
-inputHeight.value = RECT.height;
+inputHeight.value = rect.height;
 inputHeight.oninput = (e) => {
-  RECT.height = parseInt(e.target.value);
+  rect.height = parseInt(e.target.value);
   draw();
 };
 
@@ -107,28 +107,28 @@ function draw() {
   // Rectangle
   ctx.strokeStyle = "coral";
   ctx.beginPath();
-  ctx.rect(RECT.x, RECT.y, RECT.width, RECT.height);
+  ctx.rect(rect.x, rect.y, rect.width, rect.height);
   ctx.stroke();
 
   // Center of rectangle
   ctx.beginPath();
-  ctx.strokeRect(RECT.x + RECT.width / 2, RECT.y + RECT.height / 2, 1, 1);
+  ctx.strokeRect(rect.x + rect.width / 2, rect.y + rect.height / 2, 1, 1);
 
   // Circle
   ctx.strokeStyle = "teal";
   ctx.beginPath();
-  ctx.arc(CIRCLE.x, CIRCLE.y, CIRCLE.radius, 0, 2 * Math.PI);
+  ctx.arc(circle.x, circle.y, circle.radius, 0, 2 * Math.PI);
   ctx.stroke();
 
   // Center of circle
   ctx.beginPath();
-  ctx.rect(CIRCLE.x, CIRCLE.y, 1, 1);
+  ctx.rect(circle.x, circle.y, 1, 1);
   ctx.stroke();
 
   // Radius of circle
   ctx.beginPath();
-  ctx.moveTo(CIRCLE.x, CIRCLE.y);
-  ctx.lineTo(CIRCLE.x - CIRCLE.radius, CIRCLE.y);
+  ctx.moveTo(circle.x, circle.y);
+  ctx.lineTo(circle.x - circle.radius, circle.y);
   ctx.stroke();
 
   drawLabels(ctx);
@@ -139,15 +139,15 @@ function drawLabels(ctx) {
   if (showLabels) {
     // Text
     ctx.fillStyle = "white";
-    ctx.fillText(`(${CIRCLE.x}, ${CIRCLE.y})`, CIRCLE.x - 25, CIRCLE.y - 5);
+    ctx.fillText(`(${circle.x}, ${circle.y})`, circle.x - 25, circle.y - 5);
     ctx.fillText(
-      `${CIRCLE.radius}`,
-      CIRCLE.x - CIRCLE.radius / 2 - 20,
-      CIRCLE.y - 5,
+      `${circle.radius}`,
+      circle.x - circle.radius / 2 - 20,
+      circle.y - 5,
     );
-    ctx.fillText(`(${RECT.x}, ${RECT.y})`, RECT.x - 25, RECT.y - 5);
-    ctx.fillText(`${RECT.height}`, RECT.x - 5, RECT.y + RECT.height / 2);
-    ctx.fillText(`${RECT.width}`, RECT.x + RECT.width / 2 - 5, RECT.y + 3);
+    ctx.fillText(`(${rect.x}, ${rect.y})`, rect.x - 25, rect.y - 5);
+    ctx.fillText(`${rect.height}`, rect.x - 5, rect.y + rect.height / 2);
+    ctx.fillText(`${rect.width}`, rect.x + rect.width / 2 - 5, rect.y + 3);
   }
 }
 
@@ -161,8 +161,8 @@ function drawIntersections(ctx) {
 }
 
 function drawLeftIntersections(ctx) {
-  const c = distSqrd(RECT, CIRCLE) - CIRCLE.radius ** 2;
-  const b = 2 * (RECT.y - CIRCLE.y);
+  const c = distSqrd(rect, circle) - circle.radius ** 2;
+  const b = 2 * (rect.y - circle.y);
   const det = b ** 2 - 4 * c;
   if (det >= 0) {
     const y0 = (1 / 2) * (-b + Math.sqrt(det));
@@ -170,38 +170,38 @@ function drawLeftIntersections(ctx) {
 
     ctx.strokeStyle = "red";
     if (
-      (RECT.height >= 0 && 0 <= y0 && y0 <= RECT.height) ||
-      (RECT.height < 0 && RECT.height <= y0 && y0 <= 0)
+      (rect.height >= 0 && 0 <= y0 && y0 <= rect.height) ||
+      (rect.height < 0 && rect.height <= y0 && y0 <= 0)
     ) {
       // Intersection
       ctx.beginPath();
-      ctx.rect(RECT.x - 3, RECT.y + y0 - 3, 6, 6);
+      ctx.rect(rect.x - 3, rect.y + y0 - 3, 6, 6);
       ctx.stroke();
 
       // Text
       ctx.fillStyle = "white";
       ctx.fillText(
-        `(${RECT.x}, ${(RECT.y + y0).toFixed(2)})`,
-        RECT.x + 6,
-        RECT.y + y0 + 6,
+        `(${rect.x}, ${(rect.y + y0).toFixed(2)})`,
+        rect.x + 6,
+        rect.y + y0 + 6,
       );
     }
 
     if (
-      (RECT.height >= 0 && 0 <= y1 && y1 <= RECT.height) ||
-      (RECT.height < 0 && RECT.height <= y1 && y1 <= 0)
+      (rect.height >= 0 && 0 <= y1 && y1 <= rect.height) ||
+      (rect.height < 0 && rect.height <= y1 && y1 <= 0)
     ) {
       // Intersection
       ctx.beginPath();
-      ctx.rect(RECT.x - 3, RECT.y + y1 - 3, 6, 6);
+      ctx.rect(rect.x - 3, rect.y + y1 - 3, 6, 6);
       ctx.stroke();
 
       // Text
       ctx.fillStyle = "white";
       ctx.fillText(
-        `(${RECT.x}, ${(RECT.y + y1).toFixed(2)})`,
-        RECT.x + 6,
-        RECT.y + y1 + 6,
+        `(${rect.x}, ${(rect.y + y1).toFixed(2)})`,
+        rect.x + 6,
+        rect.y + y1 + 6,
       );
     }
   }
@@ -209,9 +209,9 @@ function drawLeftIntersections(ctx) {
 
 function drawRightIntersections(ctx) {
   const c =
-    distSqrd({ x: RECT.x + RECT.width, y: RECT.y }, CIRCLE) -
-    CIRCLE.radius ** 2;
-  const b = 2 * (RECT.y - CIRCLE.y);
+    distSqrd({ x: rect.x + rect.width, y: rect.y }, circle) -
+    circle.radius ** 2;
+  const b = 2 * (rect.y - circle.y);
   const det = b ** 2 - 4 * c;
   if (det >= 0) {
     const y0 = (1 / 2) * (-b + Math.sqrt(det));
@@ -219,46 +219,46 @@ function drawRightIntersections(ctx) {
 
     ctx.strokeStyle = "red";
     if (
-      (RECT.height >= 0 && 0 <= y0 && y0 <= RECT.height) ||
-      (RECT.height < 0 && RECT.height <= y0 && y0 <= 0)
+      (rect.height >= 0 && 0 <= y0 && y0 <= rect.height) ||
+      (rect.height < 0 && rect.height <= y0 && y0 <= 0)
     ) {
       // Intersection
       ctx.beginPath();
-      ctx.rect(RECT.x + RECT.width - 3, RECT.y + y0 - 3, 6, 6);
+      ctx.rect(rect.x + rect.width - 3, rect.y + y0 - 3, 6, 6);
       ctx.stroke();
 
       // Text
       ctx.fillStyle = "white";
       ctx.fillText(
-        `(${RECT.x + RECT.width}, ${(RECT.y + y0).toFixed(2)})`,
-        RECT.x + RECT.width + 6,
-        RECT.y + y0 + 6,
+        `(${rect.x + rect.width}, ${(rect.y + y0).toFixed(2)})`,
+        rect.x + rect.width + 6,
+        rect.y + y0 + 6,
       );
     }
 
     if (
-      (RECT.height >= 0 && 0 <= y1 && y1 <= RECT.height) ||
-      (RECT.height < 0 && RECT.height <= y1 && y1 <= 0)
+      (rect.height >= 0 && 0 <= y1 && y1 <= rect.height) ||
+      (rect.height < 0 && rect.height <= y1 && y1 <= 0)
     ) {
       // Intersection
       ctx.beginPath();
-      ctx.rect(RECT.x + RECT.width - 3, RECT.y + y1 - 3, 6, 6);
+      ctx.rect(rect.x + rect.width - 3, rect.y + y1 - 3, 6, 6);
       ctx.stroke();
 
       // Text
       ctx.fillStyle = "white";
       ctx.fillText(
-        `(${RECT.x + RECT.width}, ${(RECT.y + y1).toFixed(2)})`,
-        RECT.x + RECT.width + 6,
-        RECT.y + y1 + 6,
+        `(${rect.x + rect.width}, ${(rect.y + y1).toFixed(2)})`,
+        rect.x + rect.width + 6,
+        rect.y + y1 + 6,
       );
     }
   }
 }
 
 function drawTopIntersections(ctx) {
-  const c = distSqrd(RECT, CIRCLE) - CIRCLE.radius ** 2;
-  const b = 2 * (RECT.x - CIRCLE.x);
+  const c = distSqrd(rect, circle) - circle.radius ** 2;
+  const b = 2 * (rect.x - circle.x);
   const det = b ** 2 - 4 * c;
   if (det >= 0) {
     const x0 = (1 / 2) * (-b + Math.sqrt(det));
@@ -266,38 +266,38 @@ function drawTopIntersections(ctx) {
 
     ctx.strokeStyle = "red";
     if (
-      (RECT.width >= 0 && 0 <= x0 && x0 <= RECT.width) ||
-      (RECT.width < 0 && RECT.width <= x0 && x0 <= 0)
+      (rect.width >= 0 && 0 <= x0 && x0 <= rect.width) ||
+      (rect.width < 0 && rect.width <= x0 && x0 <= 0)
     ) {
       // Intersection
       ctx.beginPath();
-      ctx.rect(RECT.x + x0 - 3, RECT.y - 3, 6, 6);
+      ctx.rect(rect.x + x0 - 3, rect.y - 3, 6, 6);
       ctx.stroke();
 
       // Text
       ctx.fillStyle = "white";
       ctx.fillText(
-        `(${(RECT.x + x0).toFixed(2)}, ${RECT.y})`,
-        RECT.x + x0 + 6,
-        RECT.y + 6,
+        `(${(rect.x + x0).toFixed(2)}, ${rect.y})`,
+        rect.x + x0 + 6,
+        rect.y + 6,
       );
     }
 
     if (
-      (RECT.width >= 0 && 0 <= x1 && x1 <= RECT.width) ||
-      (RECT.width < 0 && RECT.width <= x1 && x1 <= 0)
+      (rect.width >= 0 && 0 <= x1 && x1 <= rect.width) ||
+      (rect.width < 0 && rect.width <= x1 && x1 <= 0)
     ) {
       // Intersection
       ctx.beginPath();
-      ctx.rect(RECT.x + x1 - 3, RECT.y - 3, 6, 6);
+      ctx.rect(rect.x + x1 - 3, rect.y - 3, 6, 6);
       ctx.stroke();
 
       // Text
       ctx.fillStyle = "white";
       ctx.fillText(
-        `(${(RECT.x + x1).toFixed(2)}, ${RECT.y})`,
-        RECT.x + x1 + 6,
-        RECT.y + 6,
+        `(${(rect.x + x1).toFixed(2)}, ${rect.y})`,
+        rect.x + x1 + 6,
+        rect.y + 6,
       );
     }
   }
@@ -305,9 +305,9 @@ function drawTopIntersections(ctx) {
 
 function drawBottomIntersections(ctx) {
   const c =
-    distSqrd({ x: RECT.x, y: RECT.y + RECT.height }, CIRCLE) -
-    CIRCLE.radius ** 2;
-  const b = 2 * (RECT.x - CIRCLE.x);
+    distSqrd({ x: rect.x, y: rect.y + rect.height }, circle) -
+    circle.radius ** 2;
+  const b = 2 * (rect.x - circle.x);
   const det = b ** 2 - 4 * c;
   if (det >= 0) {
     const x0 = (1 / 2) * (-b + Math.sqrt(det));
@@ -315,38 +315,38 @@ function drawBottomIntersections(ctx) {
 
     ctx.strokeStyle = "red";
     if (
-      (RECT.width >= 0 && 0 <= x0 && x0 <= RECT.width) ||
-      (RECT.width < 0 && RECT.width <= x0 && x0 <= 0)
+      (rect.width >= 0 && 0 <= x0 && x0 <= rect.width) ||
+      (rect.width < 0 && rect.width <= x0 && x0 <= 0)
     ) {
       // Intersection
       ctx.beginPath();
-      ctx.rect(RECT.x + x0 - 3, RECT.y + RECT.height - 3, 6, 6);
+      ctx.rect(rect.x + x0 - 3, rect.y + rect.height - 3, 6, 6);
       ctx.stroke();
 
       // Text
       ctx.fillStyle = "white";
       ctx.fillText(
-        `(${(RECT.x + x0).toFixed(2)}, ${RECT.y + RECT.height})`,
-        RECT.x + x0 + 6,
-        RECT.y + RECT.height + 6,
+        `(${(rect.x + x0).toFixed(2)}, ${rect.y + rect.height})`,
+        rect.x + x0 + 6,
+        rect.y + rect.height + 6,
       );
     }
 
     if (
-      (RECT.width >= 0 && 0 <= x1 && x1 <= RECT.width) ||
-      (RECT.width < 0 && RECT.width <= x1 && x1 <= 0)
+      (rect.width >= 0 && 0 <= x1 && x1 <= rect.width) ||
+      (rect.width < 0 && rect.width <= x1 && x1 <= 0)
     ) {
       // Intersection
       ctx.beginPath();
-      ctx.rect(RECT.x + x1 - 3, RECT.y + RECT.height - 3, 6, 6);
+      ctx.rect(rect.x + x1 - 3, rect.y + rect.height - 3, 6, 6);
       ctx.stroke();
 
       // Text
       ctx.fillStyle = "white";
       ctx.fillText(
-        `(${(RECT.x + x1).toFixed(2)}, ${RECT.y + RECT.height})`,
-        RECT.x + x1 + 6,
-        RECT.y + RECT.height + 6,
+        `(${(rect.x + x1).toFixed(2)}, ${rect.y + rect.height})`,
+        rect.x + x1 + 6,
+        rect.y + rect.height + 6,
       );
     }
   }
